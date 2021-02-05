6e3a352bd6238cf9e670d4c69bced09144cf63f3
Raul <raulpaezbrenes6@gmail.com>
2021-02-05 22:13:13 +0100

    Inicio de repositorio desde Ubuntu

---
+++ adios.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Adios en PHP</title>
+  </head>
+  <body>
+    <?php echo "Hasta luego desarrollador!!!"; ?>
+  </body>
+</html>
